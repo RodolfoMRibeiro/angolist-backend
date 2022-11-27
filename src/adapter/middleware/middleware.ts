8e52c091064c28b45f9c9e54a0aa1ef5ea56490b
rodolfo.ribeiro_q2pa <rodolfo.ribeiro@q2pay.com.br>
2022-11-27 14:35:29 -0300

    refactor: remove WebToken class and transformed this into middleware class

--- src/adapter/middleware/middleware.ts
+++ src/adapter/middleware/middleware.ts
@@ -1,3 +1,37 @@
 import * as express from 'express';
+import jwt from 'jsonwebtoken';
+import { Login } from '../../common/types/login';
 
-export class Middleware {}
+export class Middleware {
+  public static generateAccessToken = (login: Login): string => {
+    try {
+      return jwt.sign(login, process.env.TOKEN_SECRET as jwt.Secret, {
+        expiresIn: '2h',
+      });
+    } catch (e) {
+      return '';
+    }
+  };
+
+  public static authenticateToken = (
+    request: express.Request,
+    response: express.Response,
+    nextFunction: express.NextFunction,
+  ): express.Response | void => {
+    const authHeader = request.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) {
+      const unauthorizedStatusCode = 401;
+      return response.sendStatus(unauthorizedStatusCode);
+    }
+
+    jwt.verify(token, process.env.TOKEN_SECRET as string, (error) => {
+      if (error != null) {
+        const forbiddenStatusCode = 403;
+        return response.sendStatus(forbiddenStatusCode);
+      }
+    });
+    nextFunction();
+  };
+}
--- test/middleware/jsonWebToken.test.ts
+++ test/middleware/jsonWebToken.test.ts
@@ -1,16 +1,18 @@
-import { WebToken } from '../../src/adapter/middleware/jsonWebToken';
+import { Middleware } from '../../src/adapter/middleware/middleware';
 
 describe('generate json web token from secret_token', () => {
   const empty_string = '';
   const mockedUsername = { email: 'teste@test.com', password: '' };
 
   test('json web token successfully generated', () => {
-    expect(WebToken.generateAccessToken(mockedUsername)).not.toBe(empty_string);
+    expect(Middleware.generateAccessToken(mockedUsername)).not.toBe(
+      empty_string,
+    );
   });
 
   test('json web token failed to generate access token because of secret token', () => {
     process.env.TOKEN_SECRET = empty_string;
-    expect(WebToken.generateAccessToken(mockedUsername)).toStrictEqual(
+    expect(Middleware.generateAccessToken(mockedUsername)).toStrictEqual(
       empty_string,
     );
   });
