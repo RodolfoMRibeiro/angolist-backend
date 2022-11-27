253119eee3c8497ccfc7bd295c547a69cef6b9bd
rodolfo.ribeiro_q2pa <rodolfo.ribeiro@q2pay.com.br>
2022-11-27 10:20:58 -0300

    feature: create function to authenticate json web token sent in request's header

--- src/adapter/middleware/jsonWebToken.ts
+++ src/adapter/middleware/jsonWebToken.ts
@@ -1,9 +1,32 @@
+import * as express from 'express';
 import jwt from 'jsonwebtoken';
 
-export class Jwt {
-  private static _TOKEN_SECRET: jwt.Secret = process.env.TOKEN_SECRET ?? '';
+export class WebToken {
+  public static generateAccessToken = (username: string): string => {
+    console.log(process.env.TOKEN_SECRET);
+    return jwt.sign(username, process.env.TOKEN_SECRET as jwt.Secret);
+  };
 
-  public static generateAccessToken(username: string): string {
-    return jwt.sign(username, this._TOKEN_SECRET);
-  }
+  public static authenticateToken = (
+    request: express.Request,
+    response: express.Response,
+    nextFunction: express.NextFunction,
+  ): express.Response | void => {
+    const authHeader = request.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) {
+      const unauthorizedStatusCode = 401;
+      return response.sendStatus(unauthorizedStatusCode);
+    }
+
+    jwt.verify(token, process.env.TOKEN_SECRET as string, (error) => {
+      if (error == null) {
+        const forbiddenStatusCode = 403;
+        return response.sendStatus(forbiddenStatusCode);
+      }
+    });
+
+    nextFunction();
+  };
 }
--- test/middleware/jsonWebToken.test.ts
+++ test/middleware/jsonWebToken.test.ts
@@ -1,10 +1,10 @@
-import { Jwt } from '../../src/adapter/middleware/jsonWebToken';
+import { WebToken } from '../../src/adapter/middleware/jsonWebToken';
 
 describe('generate json web token from secret_token', () => {
   const empty_string = '';
   const mockedUsername = 'mathias';
 
   test('json web token successfully generated', () => {
-    expect(Jwt.generateAccessToken(mockedUsername)).not.toBe(empty_string);
+    expect(WebToken.generateAccessToken(mockedUsername)).not.toBe(empty_string);
   });
 });
