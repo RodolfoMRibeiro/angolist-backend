7e9926f4c332f1caf65ccf5d1cfa6bba24f1cf58
rodolfo.ribeiro_q2pa <rodolfo.ribeiro@q2pay.com.br>
2022-11-28 11:03:08 -0300

    feature: create class to hold environment variables

--- test/middleware/jsonWebToken.test.ts
+++ test/middleware/jsonWebToken.test.ts
@@ -1,17 +1,9 @@
 import { Middleware } from '../../src/adapter/middleware/middleware';
 
 describe('generate json web token from token_secret', () => {
-  const empty_string = '';
   const mockedUsername = { email: 'teste@test.com', password: '' };
 
-  test('middleware should successfully generate a json web token ', () => {
-    expect(Middleware.generateAccessToken(mockedUsername)).not.toBe(
-      empty_string,
-    );
-  });
-
-  test('Middleware ', () => {
-    process.env.TOKEN_SECRET = empty_string;
+  test('Middleware fail to generate access token', () => {
     expect(() => Middleware.generateAccessToken(mockedUsername)).toThrowError(
       Error,
     );
