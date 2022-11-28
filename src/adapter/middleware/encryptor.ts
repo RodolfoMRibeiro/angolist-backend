50c82e17d15432cb898ea3856c7a01afddc65205
rodolfo.ribeiro_q2pa <rodolfo.ribeiro@q2pay.com.br>
2022-11-28 13:31:33 -0300

    feature: create func to compare hash and password

--- src/adapter/middleware/encryptor.ts
+++ src/adapter/middleware/encryptor.ts
@@ -15,4 +15,20 @@ export class Encryptor {
 
     return hashedPassword;
   };
+
+  public static IsSamePassword = async (
+    currentPassword: string,
+    encryptedPassword: string,
+  ): Promise<boolean> => {
+    const isSamePassword = bcrypt
+      .compare(currentPassword, encryptedPassword)
+      .then((result) => {
+        return result;
+      })
+      .catch((err) => {
+        throw new Error(`COULD NOT VALIDATE PASSWORD: ${err}`);
+      });
+
+    return isSamePassword;
+  };
 }
--- test/middleware/encryptor.test.ts
+++ test/middleware/encryptor.test.ts
@@ -1,10 +1,34 @@
 import { Encryptor } from '../../src/adapter/middleware/encryptor';
 
-describe('Encryptor', () => {
+describe('Encryptor --> HashPassword', () => {
   test('encrypt password using HashPassword functions', async () => {
     const commonPassword = 'normal_password';
     const encryptedPassword = await Encryptor.HashPassword(commonPassword);
 
     expect(encryptedPassword).not.toStrictEqual(commonPassword);
+  });
+});
+
+describe('Encryptor --> IsSamePassword', () => {
+  const commonPassword = 'normal_password';
+
+  test('compare encrypted password with unencrypted password --> Success', async () => {
+    const encryptedPassword = await Encryptor.HashPassword(commonPassword);
+    const expectedBoolean = await Encryptor.IsSamePassword(
+      commonPassword,
+      encryptedPassword,
+    );
+
+    expect(expectedBoolean).toBeTruthy();
+  });
+
+  test('compare encrypted password with unencrypted password --> Fail', async () => {
+    const encryptedPassword = await Encryptor.HashPassword(commonPassword);
+    const expectedBoolean = await Encryptor.IsSamePassword(
+      encryptedPassword,
+      commonPassword,
+    );
+
+    expect(expectedBoolean).toBeFalsy();
   });
 });
