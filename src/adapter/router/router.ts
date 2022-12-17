0010bea9d3c288c6dadf98152efdf6ece80d9efc
RodolfoMRibeiro <89111970+RodolfoMRibeiro@users.noreply.github.com>
2022-12-17 12:05:56 -0300

    refactor: change routes constants

--- src/adapter/router/router.ts
+++ src/adapter/router/router.ts
@@ -1,6 +1,7 @@
 import { IRouter, Router } from 'express';
 import { Builder } from '../api/builders/userBuilder';
 import { IUserController } from '../../common/models/interfaces/IUserController';
+import { RegistrationRoutes } from '../../common/util/constants/routes';
 
 export class ApiRouter {
   private static _router: IRouter = Router();
@@ -12,9 +13,10 @@ export class ApiRouter {
 
   private static _loadRegistrationRoutes(): void {
     const controller: IUserController = Builder.NewUserController();
+    const routes = RegistrationRoutes;
 
-    this._router.post('/registration/create', (res, resp) => {controller.Create(res, resp)});
-    this._router.post('/registration/login', (res, resp) => {controller.Login(res, resp)});
-    this._router.put('registration/update', (res, resp) => {controller.Update(res, resp)});
+    this._router.post(routes.CREATE_USER, (res, resp) => {controller.Create(res, resp)});
+    this._router.put(routes.UPDATE_USER, (res, resp) => {controller.Update(res, resp)});
+    this._router.post(routes.LOGIN, (res, resp) => {controller.Login(res, resp)});
   }
 }
